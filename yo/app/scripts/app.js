64b15d9de77e57712bc571dd0dddd8186a61fc98
Olivier Barais <barais@irisa.fr>
2013-09-05 22:29:14 +0200

    improve code

--- yo/app/scripts/app.js
+++ yo/app/scripts/app.js
@@ -1,14 +1,10 @@
 'use strict';
 
-angular.module('yoApp', ['ui.calendar'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
-
+angular.module('yoApp', [ 'ui.calendar' ]).config(function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'views/main.html',
+		controller : 'MainCtrl'
+	}).otherwise({
+		redirectTo : '/'
+	});
+});
